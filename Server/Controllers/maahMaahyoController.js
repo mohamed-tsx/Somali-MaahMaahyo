a9200fede41b9305f02b3a6f2ad6d0d546c610d3
Mohamed <mohammedmohamuud.01@gmail.com>
2024-03-21 22:05:01 +0300

    Made the maahmaahyo model a unique and added a checkpoint for the maahmaahyo

--- Server/Controllers/maahMaahyoController.js
+++ Server/Controllers/maahMaahyoController.js
@@ -13,6 +13,21 @@ const addMaahMaahyo = asyncHandler(async (req, res) => {
     res.status(403);
     throw new Error("Fadlan qoraal soo geli!");
   }
+
+  const existingMaahMaahyo = await Prisma.maahMaahyo.findUnique({
+    where: {
+      maahmaahyo: maahmaahyo,
+    },
+  });
+
+  if (existingMaahMaahyo) {
+    res.status(409).json({
+      success: false,
+      message: "Maahmaahyadan wey xareysantahay fadlan mid cusub soo geli.",
+      existingMaahMaahyo,
+    });
+    return;
+  }
   const newMaahMaahyo = await Prisma.maahMaahyo.create({
     data: {
       maahmaahyo,
