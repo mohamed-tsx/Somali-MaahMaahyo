049102b69862443efef13cccf8cb3d74de6ca4cf
Mohamed <mohammedmohamuud.01@gmail.com>
2024-03-24 22:18:08 +0300

    Added the error Handler into the server file

--- Server/Server.js
+++ Server/Server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 require("dotenv").config();
 const maahmaayoRoutes = require("./Routes/maaMaahyoRoutes");
+const errorHandler = require("./Utils/errorHandler");
 
 const server = express();
 server.use(express.json());
@@ -9,6 +10,7 @@ server.use(express.urlencoded({ extended: false }));
 const PORT = process.env.PORT;
 
 server.use("/api/v1/maahmaayo/", maahmaayoRoutes);
+server.use(errorHandler());
 
 server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
